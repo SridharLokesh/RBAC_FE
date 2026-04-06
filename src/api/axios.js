127db483c5fbaf3f9bc2cbe746220422e5747221
Lokesh S <lokesh.s@meteoros.in>
2026-04-06 14:53:06 +0530

    updated url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://rbac-be-two.vercel.app/",
+  baseURL: "https://rbac-be-two.vercel.app/api",
 });
 
 // Attach token to every request automatically
